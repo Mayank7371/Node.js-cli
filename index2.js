267c779d0c3dfa6285cb81967191154fa512e7d8
Mayank7371 <neprone.dev@gmail.com>
2024-08-31 17:33:07 +0530

    add the logic

---
+++ index2.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+function main(fileName) {
+  fs.readFile(fileName, "utf-8", (err, data) => {
+    total = 0;
+    for (let index = 0; index < data.length; index++) {
+      if (data[index] === " ") {
+        total++;
+      }
+    }
+    console.log(total + 1);
+  });
+}
+main("a.txt");
