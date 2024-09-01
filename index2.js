6ac9a0f136f296098f8747b369bd4afcc0a7d333
Mayank <166874794+Mayank7371@users.noreply.github.com>
2024-09-01 16:42:01 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,15 +1,30 @@
 const fs = require("fs");
-function main(fileName) {
-  fs.readFile(fileName, "utf-8", (err, data) => {
-    total = 0;
-    for (let index = 0; index < data.length; index++) {
-      if (data[index] === " ") {
-        total++;
-      }
-    }
-    console.log(total + 1);
-  });
-}
-main(process.argv[2]);
+const { Command } = require("commander");
+const program = new Command();
 
-// console.log(process.argv);
+program
+  .name("counter")
+  .description("CLI to do file based tasks")
+  .version("0.8.0");
+
+program
+  .command("count")
+  .description("Count the number of words in a file")
+  .argument("<file>", "file to count")
+  .action((file) => {
+    fs.readFile(file, "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        let words = 0;
+        for (let i = 0; i < data.length; i++) {
+          if (data[i] === " ") {
+            words++;
+          }
+        }
+        console.log(`There are ${words + 1} words in ${file}`);
+      }
+    });
+  });
+
+program.parse();
